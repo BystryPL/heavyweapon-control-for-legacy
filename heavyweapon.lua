312ebbdc74e95114187266cbe14b4c99dde6a309
BystryPL <45846431+BystryPL@users.noreply.github.com>
2020-03-30 01:31:44 +0200

    Added chat message and changed CVARs for ETL compatibility

--- heavyweapon.lua
+++ heavyweapon.lua
@@ -8,6 +8,7 @@ IRC: #nitmod @ freenode
 
 Thanks for that!
 Edited by Micha!
+Edited by Bystry 30.03.2020 - ETL compatibility and added chat message.
 
 Contact:
 --------------------
@@ -30,12 +31,18 @@ countspectators = false		--include spectators on the player counter
 
 unlockvalue = 20		--player amount needed to unlock the following weapon values (must be greater then 13)
 
-panzer 		  = 1			--weapon amount which will be unlocked if playeramount greater then unlockvalue
-mines	 	    = 8
-flamer 		  = 1
-mortar 		  = 2
-mg42 		    = 1
+panzer 		= 1		--weapon amount which will be unlocked if playeramount greater then unlockvalue
+mines	 	= 6
+flamer 		= 0
+mortar 		= 0
+mg42 		= 1
 riflegnade 	= 2
+
+message		= "^5There are 10 or more players on the server, heavy weapons, 1 rifle granade and 4 landmines are now ^1enabled^5!"
+message2	= "^5There are less than 10 players on the server, heavy weapons, rifle grenades and landmines are now ^1disabled^5!"
+message3	= "^5There are 16 or more players on the server, 2 rifle grenades and 5 landmines are now enabled!"
+message4	= "^5There are 20 or more players on the server, 2 rifle grenades and 6 landmines are now enabled!"
+location 	= "chat"
 
 -------------------------------------------------------------------------------------
 -------------------------------CONFIG END--------------------------------------------
@@ -47,7 +54,7 @@ riflegnade 	= 2
 -------------------------------------------------------------------------------------
 
 -------//-----------------DO NOT CHANGE THESE VARS-----------------------------------
-Version = "0.1"
+Version = "0.2"
 Modname = "heavyweapon"
 
 et.CS_PLAYERS = 689
@@ -96,45 +103,49 @@ function AdjustCvarsValues( numclients )
 			end
 			et.gentity_set(j, "ps.ammoclip", 5, 0) 		--WP_PANZERFAUST
 			et.gentity_set(j, "ps.ammoclip", 6, 0) 		--WP_FLAMETHROWER
-			et.gentity_set(j, "ps.ammo", 9, 0)			  --WP_GRENADE_PINEAPPLE
+			et.gentity_set(j, "ps.ammo", 9, 0)		--WP_GRENADE_PINEAPPLE
 			et.gentity_set(j, "ps.ammoclip", 9, 0)		--WP_GRENADE_PINEAPPLE
 			et.gentity_set(j, "ps.ammoclip", 26, 0)		--WP_LANDMINE
-			et.gentity_set(j, "ps.ammo", 31, 0) 		  --WP_MOBILE_MG42
+			et.gentity_set(j, "ps.ammo", 31, 0) 		--WP_MOBILE_MG42
 			et.gentity_set(j, "ps.ammoclip", 31, 0) 	--WP_MOBILE_MG42
-			et.gentity_set(j, "ps.ammo", 35, 0)			  --WP_MORTAR
+			et.gentity_set(j, "ps.ammo", 35, 0)		--WP_MORTAR
 			et.gentity_set(j, "ps.ammoclip", 35, 0)		--WP_MORTAR
 		end
-		et.trap_Cvar_Set("team_maxMG42s", "0")
+		et.trap_Cvar_Set("team_maxMachineguns", "0")
 		et.trap_Cvar_Set("team_maxMortars", "0")
 		et.trap_Cvar_Set("team_maxRifleGrenades", "0")
-		et.trap_Cvar_Set("team_maxPanzers", "0")
+		et.trap_Cvar_Set("team_maxRockets", "0")
 		et.trap_Cvar_Set("team_maxLandmines", "0")
 		et.trap_Cvar_Set("g_maxTeamLandmines", "0")
 		et.trap_Cvar_Set("team_maxFlamers", "0")
-	elseif (numclients >= 10 and numclients < 12) and unlockvalue > 13 then
+		et.trap_SendServerCommand(-1 , string.format('%s \"%s\"',location,message2 ))
+	elseif (numclients >= 10 and numclients < 16) and unlockvalue > 13 then
 		et.trap_Cvar_Set("team_maxMortars", "0")
-		et.trap_Cvar_Set("team_maxMG42s", "1")
-		et.trap_Cvar_Set("team_maxPanzers", "1")
+		et.trap_Cvar_Set("team_maxMachineguns", "1")
+		et.trap_Cvar_Set("team_maxRockets", "1")
 		et.trap_Cvar_Set("team_maxRifleGrenades", "1")
+		et.trap_Cvar_Set("team_maxLandmines", "4")
+		et.trap_Cvar_Set("g_maxTeamLandmines", "4")
+		et.trap_Cvar_Set("team_maxFlamers", "0")
+		et.trap_SendServerCommand(-1 , string.format('%s \"%s\"',location,message ))
+	elseif (numclients >= 16 and numclients < unlockvalue) and unlockvalue > 13 then
+		et.trap_Cvar_Set("team_maxMortars", "0")
+		et.trap_Cvar_Set("team_maxMachineguns", "1")
+		et.trap_Cvar_Set("team_maxRockets", "1")
+		et.trap_Cvar_Set("team_maxRifleGrenades", "2")
 		et.trap_Cvar_Set("team_maxLandmines", "5")
 		et.trap_Cvar_Set("g_maxTeamLandmines", "5")
 		et.trap_Cvar_Set("team_maxFlamers", "0")
-	elseif (numclients >= 12 and numclients < unlockvalue) and unlockvalue > 13 then
-		et.trap_Cvar_Set("team_maxMortars", "1")
-		et.trap_Cvar_Set("team_maxMG42s", "1")
-		et.trap_Cvar_Set("team_maxPanzers", "1")
-		et.trap_Cvar_Set("team_maxRifleGrenades", "1")
-		et.trap_Cvar_Set("team_maxLandmines", "5")
-		et.trap_Cvar_Set("g_maxTeamLandmines", "5")
-		et.trap_Cvar_Set("team_maxFlamers", "1")
+		et.trap_SendServerCommand(-1 , string.format('%s \"%s\"',location,message3 ))
 	elseif (numclients >= unlockvalue) and unlockvalue > 13 then
-		et.trap_Cvar_Set("team_maxMG42s", mg42)
+		et.trap_Cvar_Set("team_maxMachineguns", mg42)
 		et.trap_Cvar_Set("team_maxMortars", mortar)
 		et.trap_Cvar_Set("team_maxRifleGrenades", riflegnade)
-		et.trap_Cvar_Set("team_maxPanzers", panzer)
+		et.trap_Cvar_Set("team_maxRockets", panzer)
 		et.trap_Cvar_Set("team_maxLandmines", mines)
 		et.trap_Cvar_Set("g_maxTeamLandmines", mines)
 		et.trap_Cvar_Set("team_maxFlamers", flamer)
+		et.trap_SendServerCommand(-1 , string.format('%s \"%s\"',location,message4 ))
 	end
 
 end
